ab97f05cabf0ed6bbb42e8a0d1f108fe53c43ae7
Vini Fraga <vinifragam@gmail.com>
2025-07-23 17:34:56 -0300

    Mock do dialog de "Criar álbum" - Adaptando ImagePreview para ser selecionável

--- src/contexts/albums/components/album-new-dialog.tsx
+++ src/contexts/albums/components/album-new-dialog.tsx
@@ -13,7 +13,7 @@ import Text from "../../../components/text";
 import type { Photo } from "../../photos/models/photo";
 import SelectCheckboxIllustration from "../../../assets/images/select-checkbox.svg?react";
 import Skeleton from "../../../components/skeleton";
-import ImagePreview from "../../../components/image-preview";
+import PhotoImageSelectable from "../../photos/components/photo-image-selectable";
 
 interface AlbumNewDialogProps {
   trigger: React.ReactNode;
@@ -44,6 +44,10 @@ export default function AlbumNewDialog({ trigger }: AlbumNewDialogProps) {
     },
   ];
 
+  function handleTogglePhoto(selected: boolean, photoId: string) {
+    console.log(selected, photoId);
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild>{trigger}</DialogTrigger>
@@ -61,11 +65,14 @@ export default function AlbumNewDialog({ trigger }: AlbumNewDialogProps) {
             {!isLoadingPhotos && photos.length > 0 && (
               <div className="flex flex-wrap gap-2">
                 {photos.map((photo) => (
-                  <ImagePreview
+                  <PhotoImageSelectable
                     key={photo.id}
                     src={`/images/${photo.imageId}`}
                     title={photo.title}
-                    className="w-20 h-20 rounded"
+                    imageClassName="w-20 h-20"
+                    onSelectImage={(selected) =>
+                      handleTogglePhoto(selected, photo.id)
+                    }
                   />
                 ))}
               </div>
@@ -76,7 +83,7 @@ export default function AlbumNewDialog({ trigger }: AlbumNewDialogProps) {
                 {Array.from({ length: 4 }).map((_, index) => (
                   <Skeleton
                     key={`photo-loading-${index}`}
-                    className="w-20 h-20 rounded"
+                    className="w-20 h-20 rounded-lg"
                   />
                 ))}
               </div>
