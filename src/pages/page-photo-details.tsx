8c43671c0cf75ea3cb763a01b1aae22ad043b4fe
Vini Fraga <vinifragam@gmail.com>
2025-07-23 16:35:11 -0300

    Mock da página de detalhes da foto - Imagem e botão de excluir

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -3,6 +3,8 @@ import Text from "../components/text";
 import type { Photo } from "../contexts/photos/models/photo";
 import Skeleton from "../components/skeleton";
 import PhotosNavigator from "../contexts/photos/components/photos-navigator";
+import ImagePreview from "../components/image-preview";
+import Button from "../components/button";
 
 export default function PagePhotoDetails() {
   const isLoadingPhoto = false;
@@ -28,6 +30,26 @@ export default function PagePhotoDetails() {
 
         <PhotosNavigator />
       </header>
+
+      <div className="grid grid-cols-[21rem] gap-24">
+        <div className="space-y-3">
+          {!isLoadingPhoto ? (
+            <ImagePreview
+              src={`/images/${photo?.imageId}`}
+              title={photo.title}
+              imageClassName="h-[21rem]"
+            />
+          ) : (
+            <Skeleton className="h-[21rem]" />
+          )}
+
+          {!isLoadingPhoto ? (
+            <Button variant="destructive">Excluir</Button>
+          ) : (
+            <Skeleton className="w-20 h-10" />
+          )}
+        </div>
+      </div>
     </Container>
   );
 }
