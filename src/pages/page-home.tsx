6386c19f90f466db118a1097230429d946d50caa
Vini Fraga <vinifragam@gmail.com>
2025-07-23 12:16:57 -0300

    Componente PhotoWidget

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,9 +1,49 @@
-import Text from "../components/text";
+import Container from "../components/container";
+import PhotoWidget from "../contexts/photos/components/photo-widget";
+import type { Photo } from "../contexts/photos/models/photo";
 
 export default function PageHome() {
   return (
-    <>
-      <Text variant="heading-medium">Página inicial</Text>
-    </>
+    <Container>
+      <div className="grid grid-cols-4 gap-9">
+        <PhotoWidget
+          photo={{
+            id: "123",
+            title: "Olá mundo!",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "321", title: "Album 1" },
+              { id: "321", title: "Album 2" },
+              { id: "321", title: "Album 3" },
+            ],
+          }}
+        />
+        <PhotoWidget
+          photo={{
+            id: "123",
+            title: "Olá mundo!",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "321", title: "Album 1" },
+              { id: "321", title: "Album 2" },
+              { id: "321", title: "Album 3" },
+            ],
+          }}
+        />
+        <PhotoWidget
+          photo={{
+            id: "123",
+            title: "Olá mundo!",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "321", title: "Album 1" },
+              { id: "321", title: "Album 2" },
+              { id: "321", title: "Album 3" },
+            ],
+          }}
+        />
+        <PhotoWidget photo={{} as Photo} loading />
+      </div>
+    </Container>
   );
 }
