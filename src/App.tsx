4e5a99d271f88330b596326b1d0bac5de71359a0
Vini Fraga <vinifragam@gmail.com>
2025-07-23 10:09:52 -0300

    Definindo comportamento do componente InputSingleFile - Alternando estados

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,11 @@ import InputText from "./components/input-text";
 import SearchIcon from "./assets/icons/search.svg?react";
 import InputCheckbox from "./components/input-checkbox";
 import InputSingleFile from "./components/input-single-file";
+import { useForm } from "react-hook-form";
 
 export default function App() {
+  const form = useForm();
+
   return (
     <div className="grid gap-7 p-6">
       <div className="flex gap-3">
@@ -62,7 +65,7 @@ export default function App() {
       </div>
 
       <div>
-        <InputSingleFile />
+        <InputSingleFile form={form} {...form.register("file")} />
       </div>
     </div>
   );
