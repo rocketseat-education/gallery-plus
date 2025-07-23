b6ac76e9b09289d52c116edb8c1e0bd133438207
Vini Fraga <vinifragam@gmail.com>
2025-07-23 14:59:01 -0300

    Mock da lista de fotos

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,13 +1,12 @@
 import Container from "../components/container";
-import PhotoWidget from "../contexts/photos/components/photo-widget";
-import type { Photo } from "../contexts/photos/models/photo";
+import PhotosList from "../contexts/photos/components/photos-list";
 
 export default function PageHome() {
   return (
     <Container>
-      <div className="grid grid-cols-4 gap-9">
-        <PhotoWidget
-          photo={{
+      <PhotosList
+        photos={[
+          {
             id: "123",
             title: "Olá mundo!",
             imageId: "portrait-tower.png",
@@ -16,34 +15,9 @@ export default function PageHome() {
               { id: "123", title: "Album 2" },
               { id: "456", title: "Album 3" },
             ],
-          }}
-        />
-        <PhotoWidget
-          photo={{
-            id: "456",
-            title: "Olá mundo!",
-            imageId: "portrait-tower.png",
-            albums: [
-              { id: "3421", title: "Album 1" },
-              { id: "123", title: "Album 2" },
-              { id: "456", title: "Album 3" },
-            ],
-          }}
-        />
-        <PhotoWidget
-          photo={{
-            id: "abc",
-            title: "Olá mundo!",
-            imageId: "portrait-tower.png",
-            albums: [
-              { id: "3421", title: "Album 1" },
-              { id: "123", title: "Album 2" },
-              { id: "456", title: "Album 3" },
-            ],
-          }}
-        />
-        <PhotoWidget photo={{} as Photo} loading />
-      </div>
+          },
+        ]}
+      />
     </Container>
   );
 }
