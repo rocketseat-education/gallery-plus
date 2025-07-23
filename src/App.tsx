fa45570700044f79ede9fbbba8e6a5f931079092
Vini Fraga <vinifragam@gmail.com>
2025-07-23 11:01:03 -0300

    Componente Dialog

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,15 @@ import InputCheckbox from "./components/input-checkbox";
 import InputSingleFile from "./components/input-single-file";
 import { useForm } from "react-hook-form";
 import ImageFilePreview from "./components/image-file-preview";
+import {
+  Dialog,
+  DialogBody,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+} from "./components/dialog";
+import { DialogClose, DialogTrigger } from "@radix-ui/react-dialog";
+import Text from "./components/text";
 
 export default function App() {
   const form = useForm();
@@ -75,6 +84,34 @@ export default function App() {
           replaceBy={<ImageFilePreview src={fileSrc} alt="Imagem" />}
           {...form.register("file")}
         />
+      </div>
+
+      <div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>Abrir Modal</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>Teste dialog</DialogHeader>
+            <DialogBody>
+              <Text as="div">Teste conteúdo do dialog</Text>
+
+              <InputSingleFile
+                allowedExtensions={["png", "jpg", "jpeg", "webp"]}
+                maxFileSizeInMB={50}
+                form={form}
+                replaceBy={<ImageFilePreview src={fileSrc} alt="Imagem" />}
+                {...form.register("file")}
+              />
+            </DialogBody>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="secondary">Cancelar</Button>
+              </DialogClose>
+              <Button>Adicionar</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
