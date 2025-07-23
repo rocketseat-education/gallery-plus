a618e1421740f9ce179699b175c9fd1544af8add
Vini Fraga <vinifragam@gmail.com>
2025-07-23 15:59:08 -0300

    Mock do filtro por álbuns

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,9 +1,19 @@
 import Container from "../components/container";
+import AlbumsFilter from "../contexts/albums/components/albums-filter";
 import PhotosList from "../contexts/photos/components/photos-list";
 
 export default function PageHome() {
   return (
     <Container>
+      <AlbumsFilter
+        albums={[
+          { id: "3421", title: "Album 1" },
+          { id: "123", title: "Album 2" },
+          { id: "456", title: "Album 3" },
+        ]}
+        className="mb-9"
+      />
+
       <PhotosList
         photos={[
           {
