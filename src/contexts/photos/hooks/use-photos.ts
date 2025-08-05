c840b4b35475647dc3c7dc6a8c7e9d1e437271db
Vini Fraga <vinifragam@gmail.com>
2025-08-05 16:56:43 -0300

    Adicionando filtro de álbum na lista de fotos

--- src/contexts/photos/hooks/use-photos.ts
+++ src/contexts/photos/hooks/use-photos.ts
@@ -1,15 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetcher } from "../../../helpers/api";
 import type { Photo } from "../models/photo";
+import { useQueryState, createSerializer, parseAsString } from "nuqs";
+
+const toSearchParams = createSerializer({
+  albumId: parseAsString,
+});
 
 export default function usePhotos() {
+  const [albumId, setAlbumId] = useQueryState("albumId");
+
   const { data, isLoading } = useQuery<Photo[]>({
-    queryKey: ["photos"],
-    queryFn: () => fetcher("/photos"),
+    queryKey: ["photos", albumId],
+    queryFn: () => fetcher(`/photos${toSearchParams({ albumId })}`),
   });
 
   return {
     photos: data || [],
     isLoadingPhotos: isLoading,
+    filters: {
+      albumId,
+      setAlbumId,
+    },
   };
 }
