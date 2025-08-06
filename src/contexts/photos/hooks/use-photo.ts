8f262ba393b4134e410f5d211d9bc1a3f424360a
Vini Fraga <vinifragam@gmail.com>
2025-08-06 17:33:24 -0300

    Novo álbum - Deletando foto

--- src/contexts/photos/hooks/use-photo.ts
+++ src/contexts/photos/hooks/use-photo.ts
@@ -4,6 +4,7 @@ import type { Photo } from "../models/photo";
 import type { PhotoNewFormSchema } from "../../schemas";
 import { toast } from "sonner";
 import usePhotoAlbums from "./use-photo-albums";
+import { useNavigate } from "react-router";
 
 interface PhotoDetailResponse extends Photo {
   nextPhotoId?: string;
@@ -11,6 +12,7 @@ interface PhotoDetailResponse extends Photo {
 }
 
 export default function usePhoto(id?: string) {
+  const navigate = useNavigate();
   const { data, isLoading } = useQuery<PhotoDetailResponse>({
     queryKey: ["photo", id],
     queryFn: () => fetcher(`/photos/${id}`),
@@ -50,11 +52,25 @@ export default function usePhoto(id?: string) {
     }
   }
 
+  async function deletePhoto(photoId: string) {
+    try {
+      await api.delete(`/photos/${photoId}`);
+
+      toast.success("Foto excluída com sucesso");
+
+      navigate("/");
+    } catch (error) {
+      toast.error("Erro ao excluir foto");
+      throw error;
+    }
+  }
+
   return {
     photo: data,
     nextPhotoId: data?.nextPhotoId,
     previousPhotoId: data?.previousPhotoId,
     isLoadingPhoto: isLoading,
     createPhoto,
+    deletePhoto,
   };
 }
