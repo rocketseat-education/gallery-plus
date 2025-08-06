04e5066cf0575e7b81b31583a51698be220d11ca
Vini Fraga <vinifragam@gmail.com>
2025-08-06 17:27:49 -0300

    Novo álbum - gerenciando fotos em álbuns

--- src/contexts/albums/hooks/use-album.ts
+++ src/contexts/albums/hooks/use-album.ts
@@ -4,10 +4,12 @@ import { api } from "../../../helpers/api";
 import type { Album } from "../models/album";
 import { useQueryClient } from "@tanstack/react-query";
 import usePhotos from "../../photos/hooks/use-photos";
+import usePhotoAlbums from "../../photos/hooks/use-photo-albums";
 
 export default function useAlbum() {
   const queryClient = useQueryClient();
   const { photos } = usePhotos();
+  const { managePhotoOnAlbum } = usePhotoAlbums();
 
   async function createAlbum(payload: AlbumNewFormSchema) {
     try {
@@ -22,9 +24,7 @@ export default function useAlbum() {
               photos
                 .find((photo) => photo.id === photoId)
                 ?.albums.map((album) => album.id) || [];
-            return api.put(`/photos/${photoId}/albums`, {
-              albumsIds: [...photoAlbumsIds, album.id],
-            });
+            return managePhotoOnAlbum(photoId, [...photoAlbumsIds, album.id]);
           })
         );
       }
--- src/contexts/photos/hooks/use-photo.ts
+++ src/contexts/photos/hooks/use-photo.ts
@@ -3,6 +3,7 @@ import { api, fetcher } from "../../../helpers/api";
 import type { Photo } from "../models/photo";
 import type { PhotoNewFormSchema } from "../../schemas";
 import { toast } from "sonner";
+import usePhotoAlbums from "./use-photo-albums";
 
 interface PhotoDetailResponse extends Photo {
   nextPhotoId?: string;
@@ -16,6 +17,7 @@ export default function usePhoto(id?: string) {
     enabled: !!id,
   });
   const queryClient = useQueryClient();
+  const { managePhotoOnAlbum } = usePhotoAlbums();
 
   async function createPhoto(payload: PhotoNewFormSchema) {
     try {
@@ -36,9 +38,7 @@ export default function usePhoto(id?: string) {
       );
 
       if (payload.albumsIds && payload.albumsIds.length > 0) {
-        await api.put(`/photos/${photo.id}/albums`, {
-          albumsIds: payload.albumsIds,
-        });
+        await managePhotoOnAlbum(photo.id, payload.albumsIds);
       }
 
       queryClient.invalidateQueries({ queryKey: ["photos"] });
