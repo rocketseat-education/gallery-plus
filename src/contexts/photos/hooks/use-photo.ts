eca9243db5e2d4d58fad8fe670204f3bfe9cc214
Vini Fraga <vinifragam@gmail.com>
2025-08-06 16:05:17 -0300

    Nova foto - criando foto via API

--- src/contexts/photos/hooks/use-photo.ts
+++ src/contexts/photos/hooks/use-photo.ts
@@ -1,6 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetcher } from "../../../helpers/api";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { api, fetcher } from "../../../helpers/api";
 import type { Photo } from "../models/photo";
+import type { PhotoNewFormSchema } from "../../schemas";
 
 interface PhotoDetailResponse extends Photo {
   nextPhotoId?: string;
@@ -13,11 +14,44 @@ export default function usePhoto(id?: string) {
     queryFn: () => fetcher(`/photos/${id}`),
     enabled: !!id,
   });
+  const queryClient = useQueryClient();
+
+  async function createPhoto(payload: PhotoNewFormSchema) {
+    // eslint-disable-next-line no-useless-catch
+    try {
+      const { data: photo } = await api.post<Photo>("/photos", {
+        title: payload.title,
+      });
+
+      await api.post(
+        `/photos/${photo.id}/image`,
+        {
+          file: payload.file[0],
+        },
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      if (payload.albumsIds && payload.albumsIds.length > 0) {
+        await api.put(`/photos/${photo.id}/albums`, {
+          albumsIds: payload.albumsIds,
+        });
+      }
+
+      queryClient.invalidateQueries({ queryKey: ["photos"] });
+    } catch (error) {
+      throw error;
+    }
+  }
 
   return {
     photo: data,
     nextPhotoId: data?.nextPhotoId,
     previousPhotoId: data?.previousPhotoId,
     isLoadingPhoto: isLoading,
+    createPhoto,
   };
 }
