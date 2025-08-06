4b9a8fadaf5e09288cbf320e7fe208e0978b26d0
Vini Fraga <vinifragam@gmail.com>
2025-08-06 16:13:34 -0300

    Nova foto - mostrando feedback com toast notification

--- src/contexts/photos/hooks/use-photo.ts
+++ src/contexts/photos/hooks/use-photo.ts
@@ -2,6 +2,7 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { api, fetcher } from "../../../helpers/api";
 import type { Photo } from "../models/photo";
 import type { PhotoNewFormSchema } from "../../schemas";
+import { toast } from "sonner";
 
 interface PhotoDetailResponse extends Photo {
   nextPhotoId?: string;
@@ -17,7 +18,6 @@ export default function usePhoto(id?: string) {
   const queryClient = useQueryClient();
 
   async function createPhoto(payload: PhotoNewFormSchema) {
-    // eslint-disable-next-line no-useless-catch
     try {
       const { data: photo } = await api.post<Photo>("/photos", {
         title: payload.title,
@@ -42,7 +42,10 @@ export default function usePhoto(id?: string) {
       }
 
       queryClient.invalidateQueries({ queryKey: ["photos"] });
+
+      toast.success("Foto crida com sucesso");
     } catch (error) {
+      toast.success("Erro ao criar foto");
       throw error;
     }
   }
